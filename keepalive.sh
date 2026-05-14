f486b7ded7ad2266da95bf77b81d2e5799d8614e
Z User <z@container>
2026-05-14 19:24:40 +0000

    chore: remove deployment scripts

--- keepalive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export NODE_ENV=production
-while true; do
-    node .next/standalone/server.js 2>&1
-    echo "[$(date)] Server exited, restarting..." >> /home/z/my-project/restart.log
-    sleep 1
-done
